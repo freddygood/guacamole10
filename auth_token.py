de25835557900a783b829595d622d61d9776fa7e
Fedor Patrakov <freddy-good@yandex.ru>
2018-10-30 23:38:20 +0100

    Refactored logging

--- auth_token.py
+++ auth_token.py
@@ -28,18 +28,14 @@ secret = getattr(config, 'secret', {})
 geoip_blacklist_default = getattr(config, 'geoip_blacklist_default', [])
 geoip_blacklist = getattr(config, 'geoip_blacklist', {})
 
-def log_param(func, prefix, param):
-	application.logger.debug("{}: {} = {}".format(func, prefix, param))
-
 def validate_timestamp(timestamp):
 	now = int(time())
-	log_param('validate_timestamp', 'now', now)
-	log_param('validate_timestamp', 'timestamp', timestamp)
+	application.logger.debug('Comparing timestamp {} with now {}'.format(timestamp, now))
 	return now < int(timestamp)
 
 @cache.memoize(timeout=cache_timeout)
-	log_param('validate_geoip', 'remote_addr', remote_addr)
 def validate_geoip(remote_addr, location):
+
 	def get_geoip_blacklist(location):
 		if location in geoip_blacklist.keys():
 			application.logger.debug("Found geoip blacklist {} for location {}".format(geoip_blacklist[location], location))
@@ -64,7 +60,8 @@ def validate_geoip(remote_addr, location):
 		application.logger.error(e)
 		return True
 
-	log_param('validate_geoip', 'country', response.country.iso_code)
+	application.logger.debug('Matched country {} for remote IP {}'.format(response.country.iso_code, remote_addr))
+
 	if response.country.iso_code not in blacklist:
 		return True
 	else:
@@ -88,28 +85,22 @@ echo "/token=nva=$nva~ip=$ip~dirs=$dirs~hash=0$token$path$file"
 
 def validate_token(token, timestamp, dirs, path, location, remote_addr = ''):
 	calculated_token = calculate_token(timestamp, dirs, path, location, remote_addr)
-	log_param('validate_token', 'token', token)
-	log_param('validate_token', 'calculated', calculated_token[0:20])
-	log_param('validate_token', 'timestamp', timestamp)
-	log_param('validate_token', 'dirs', dirs)
-	log_param('validate_token', 'path', path)
-	log_param('validate_token', 'remote_addr', remote_addr)
-
 	if (hasattr(hmac, 'compare_digest')):
 		return hmac.compare_digest(calculated_token[0:20].encode(), token.encode())
 	else:
 		return calculated_token[0:20].encode() == token.encode()
 
-def get_secret(location):
-	if location in secret.keys():
-		application.logger.debug("Found secret for location {}".format(location))
-		return secret[location]
-	else:
-		application.logger.debug("Using default secret")
-		return secret_default
-
 @cache.memoize(timeout=cache_timeout)
 def calculate_token(timestamp, dirs, path, location, remote_addr):
+
+	def get_secret(location):
+		if location in secret.keys():
+			application.logger.debug("Found secret for location {}".format(location))
+			return secret[location]
+		else:
+			application.logger.debug("Using default secret")
+			return secret_default
+
 	if remote_addr:
 		signature_line = "/{}/?nva={}&ip={}&dirs={}".format(path, timestamp, remote_addr, dirs)
 	else:
@@ -135,27 +126,20 @@ def index():
 # /lbcgrouplive/token=nva=1538337566~dirs=1~hash=004acb40fa3d37b94fdcd/lbclive.smil/playlist.m3u8
 @application.route('/<location>/token=nva=<timestamp>~dirs=<int:dirs>~hash=0<token>/<path:path>/<file>', methods=['GET'])
 def secure_link(token, timestamp, dirs, path, file, location):
-	log_param('secure_link', 'token', token)
-	log_param('secure_link', 'timestamp', timestamp)
-	log_param('secure_link', 'dirs', dirs)
-	log_param('secure_link', 'path', path)
-	log_param('secure_link', 'file', file)
-	log_param('secure_link', 'location', location)
-
 	if request.headers.getlist("X-Forwarded-For"):
 		remote_addr = request.headers.getlist("X-Forwarded-For")[0]
 	else:
 		remote_addr = request.remote_addr
-	log_param('secure_link', 'remote_addr', remote_addr)
 
 	response = Response()
 	if validate_timestamp(timestamp):
+		response.headers['X-Auth-Timestamp-Status'] = 'Valid'
 
 		if validate_geoip(remote_addr, location):
 			response.headers['X-Auth-GeoIP-Status'] = 'Valid'
 
 			if validate_token(token, timestamp, dirs, path, location):
-				response.headers['X-Auth-Token-Path'] = path + '/' + file
+				response.headers['X-Auth-Original-Path'] = path + '/' + file
 				response.headers['X-Auth-Token-Status'] = 'Valid'
 			else:
 				application.logger.warning("Token {} is invalid".format(token))
@@ -175,26 +159,20 @@ def secure_link(token, timestamp, dirs, path, file, location):
 # /lbcgrouplive/token=nva=1538337566~ip=127.0.0.1~dirs=1~hash=004acb40fa3d37b94fdcd/lbclive.smil/playlist.m3u8
 @application.route('/<location>/token=nva=<timestamp>~ip=<ip>~dirs=<int:dirs>~hash=0<token>/<path:path>/<file>', methods=['GET'])
 def secure_link_ip(token, timestamp, ip, dirs, path, file, location):
-	log_param('secure_link_ip', 'token', token)
-	log_param('secure_link_ip', 'timestamp', timestamp)
-	log_param('secure_link_ip', 'ip', ip)
-	log_param('secure_link_ip', 'dirs', dirs)
-	log_param('secure_link_ip', 'path', path)
-	log_param('secure_link_ip', 'file', file)
-	log_param('secure_link_ip' ,'location', location)
-
 	if request.headers.getlist("X-Forwarded-For"):
 		remote_addr = request.headers.getlist("X-Forwarded-For")[0]
 	else:
 		remote_addr = request.remote_addr
-	log_param('secure_link_ip', 'remote_addr', remote_addr)
 
 	response = Response()
 	if validate_timestamp(timestamp):
+		response.headers['X-Auth-Timestamp-Status'] = 'Valid'
+
 		if validate_geoip(remote_addr, location):
 			response.headers['X-Auth-GeoIP-Status'] = 'Valid'
+
 			if validate_token(token, timestamp, dirs, path, location, remote_addr):
-				response.headers['X-Auth-Token-Path'] = path + '/' + file
+				response.headers['X-Auth-Original-Path'] = path + '/' + file
 				response.headers['X-Auth-Token-Status'] = 'Valid'
 			else:
 				application.logger.warning("Token {} is invalid".format(token))
